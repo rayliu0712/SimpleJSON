2fc743c08aede39c646ce69f3909845b021aa074
Ray Liu <ray2006950712@gmail.com>
2022-09-04 16:54:18 +0800

    (2) Layout-Ready

--- app/src/main/java/com/rl/simplejson/MainActivity.kt
+++ app/src/main/java/com/rl/simplejson/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.rl.simplejson
 
 import android.content.Intent
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.Settings
 import android.widget.ArrayAdapter
+import com.google.android.material.snackbar.Snackbar
 import com.rl.simplejson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val list = mutableListOf("A","B","C")
-    private var selectedFile: String? = null
+    private var list = mutableListOf("A","B","C")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,16 +26,30 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(intent, 1024)
         }
 
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
-        binding.listview.adapter = adapter
+        /* -------------------------------------------------- */
 
-        binding.fabOpen.setOnClickListener {
+        binding.json.typeface = Typeface.createFromAsset(assets,"fonts/Consolas.ttf")
+        binding.filePath.typeface = Typeface.createFromAsset(assets,"fonts/Consolas.ttf")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        binding.ListView.adapter = adapter
+
+        binding.filePath.setOnClickListener {
+
             val intent = Intent()
                 .setType("application/json")
                 .setAction(Intent.ACTION_GET_CONTENT)
 
             startActivityForResult(Intent.createChooser(intent, "Select a file"), 256)
         }
+
+        binding.fabBack.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onBackPressed() {
+        Snackbar.make(binding.CoordinatorLayout, "Back Pressed", Snackbar.ANIMATION_MODE_SLIDE).show()
     }
 
     @Deprecated("Deprecated in Java")
@@ -42,15 +57,8 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == 256 && resultCode == RESULT_OK) {
-            selectedFile = data?.data?.path?.substring(18)
+            val selectedFile = data?.data?.path?.substring(18)
+            Snackbar.make(binding.CoordinatorLayout, "$selectedFile", Snackbar.ANIMATION_MODE_SLIDE).show()
         }
-    }
-
-    private fun load() {
-
-    }
-
-    private fun update() {
-
     }
 }
